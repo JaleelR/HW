20662c51634bdfec933f66a9619481ec757be0b7
Jaleel Whaley <112666361+JaleelR@users.noreply.github.com>
2023-05-24 23:11:44 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,35 +1,21 @@
-# Put your app here
-from flask import Flask
-from flask import request
-import operations
+from flask import Flask, render_template, request
+from stories import story
 app = Flask(__name__)
+##return Words for home 
+##for story im returning generate 
+@app.route("/")
+def home():
 
-@app.route('/add')
-def add():
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    result = operations.add(a, b)
-    return str(result)
-   
+  return render_template("home.html" )
 
-@app.route("/sub")
-def sub():
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    result = operations.sub(a, b)
-    return str(result)
-    
-   
-@app.route("/mult")
-def mult():
-    a = request.args.get('a', type=float)
-    b = request.args.get('b', type=float)
-    result = operations.mult(a, b)
-    return str(result)
 
-@app.route("/div")
-def div():
-    a = request.args.get('a', type=float)
-    b = request.args.get('b', type=float)
-    result = operations.div(a, b)
-    return str(result)
+@app.route("/story")
+def storygen(): 
+  place = request.args.get('place')
+  noun  = request.args.get('noun')
+  verb = request.args.get('verb')
+  adjective  = request.args.get('adjective')
+  pn = request.args.get('plural_noun')
+  answer = {"place": place, "noun": noun, "verb": verb, "adjective":adjective , "plural_noun":pn}
+  showstory = story.generate(answer)
+  return render_template("story.html", story = showstory)
