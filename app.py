0ec8f90af5bc76fbfac04b7473664926d17f17be
Jaleel Whaley <jw430118@gmail.com>
2023-06-05 15:27:37 -0400

    added sessions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect, request, url_for, flash
+from flask import Flask, render_template, redirect, request, url_for, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Naruto7'
@@ -9,41 +10,50 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
 
-response = []; 
 
 @app.route('/')
 def home(): 
+
     return render_template("home.html", title = satisfaction_survey.title, instructions = satisfaction_survey.instructions )
 
-
+@app.route("/responses", methods = ["POST"])
+def respone(): 
+   session['response'] = []
+   return redirect(url_for("show_question", qid = 0)) 
+   
 @app.route('/questions/<int:qid>')
-def show_question(qid):
-    qobj = satisfaction_survey.questions
+def show_question(qid):     
+    qobj = satisfaction_survey.questions  
+    response = session["response"]
+    if response is []:
+        return redirect("/")
 
     # Check if the question ID is out of bounds
     if qid > len(qobj):
         flash("Out of order!!!!!!")
-        return redirect(url_for("show_question", qid = 1 ))  # Redirect to the first question
+        return redirect(url_for("show_question", qid = len(response)))  # Redirect to the first question
         
     # Check if the user hasn't answered the previous question
-    if qid > 0 and qid - 1 > len(response): 
+    if qid > 1 and qid - 1 > len(response): 
     
          # Assuming `responses` is a list storing user responses
         return redirect(url_for("show_question",  qid = qid- 1))  # Redirect to the previous question
 
     quest = qobj[qid].question  # Adjusting index to match 0-based indexing
+    print(len(qobj))
     qlist = qobj[qid].choices
-
     return render_template("questions.html", final_question=quest, choice=qlist)
 
 
 @app.route('/answer', methods = ["POST"])
 def answer():
-    global response
+    response = session["response"]
     answer = request.form.get("answer")
     if len(response) < 4: 
       next_question_index = len(response)  
       response.append(answer)
+      session['response'] = response
+
       print(response)
       return redirect( url_for("show_question", qid=next_question_index))
     else:
@@ -54,7 +64,7 @@ def answer():
 
 
 @app.route('/thankyou')
-def thankyouu():
+def thankyou():
     return render_template("thank_you.html")
 
 # app.route('questions/<1>')
